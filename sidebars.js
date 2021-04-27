105effd8e66160b546ccbe2cd6422cf506e05b55
RKaron(jorsek) <rafal.karon@Jorsek.com>
2021-04-27 09:35:03 +0200

    gitignore update

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,13 @@ module.exports = {
   someSidebar: {
     Docusaurus: ["doc1", "doc2", "doc3"],
     Features: ["mdx"],
+    Jorsek: [
+      "7b095a50-a34a-11eb-b323-02429593e86b",
+      "8def4df0-a34a-11eb-b323-02429593e86b",
+      "7106c020-a349-11eb-b323-02429593e86b",
+      "8391cd10-a34a-11eb-b323-02429593e86b",
+      "b4bc9b70-a351-11eb-b323-02429593e86b",
+      "b4da5ca0-a351-11eb-b323-02429593e86b",
+    ],
   },
 };
